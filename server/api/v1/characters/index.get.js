6233e77e9e630d3b23ae5d4cfb7daaa48346d7d7
Jason Gorst <jason.gorst@me.com>
2024-11-12 00:44:54 -0600

    add query to characters api call

--- server/api/v1/characters/index.get.js
+++ server/api/v1/characters/index.get.js
@@ -1,8 +1,12 @@
-export default defineEventHandler(async () => {
+export default defineEventHandler(async (event) => {
+  const query = getQuery(event)
   let characters
 
   try {
-    characters = await $fetch("http://localhost:3000/characters")
+    characters = await $fetch(
+      "http://localhost:3000/characters",
+      { query: query }
+    )
   } catch(error) {
     throw createError({
       statusCode: 500,
