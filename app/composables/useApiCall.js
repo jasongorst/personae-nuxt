f25720bb9cfda817048eaaa3b2925665d3f33999
Jason Gorst <jason.gorst@me.com>
2025-01-12 23:31:48 -0600

    match useApiCall callback options to useFetch interceptors

--- app/composables/useApiCall.js
+++ app/composables/useApiCall.js
@@ -1,65 +1,52 @@
 export function useApiCall(
   url,
   {
-    manualFetch = false,
-    beforeCb = () => {},
-    successCb = () => {},
-    fieldErrorCb = () => {},
-    apiErrorCb = () => {},
-    fetchErrorCb = () => {},
-    ...fetchOptions
+    manual = false,
+    beforeCb = _noop,
+    successCb = _noop,
+    fieldErrorCb = _noop,
+    apiErrorCb = _noop,
+    fetchErrorCb = _noop,
+    ...additionalOptions
   } = {}
 ) {
-  const options = {
-    onRequest: async ({ request, options }) => {
-      //console.log("[useFetch request]")
-      //console.dir(request, { colors: true })
-      //console.dir(options, { colors: true })
+  const useFetchOptions = {
+    onRequest: [
+      // merge options from useApiCall
+      ({ options }) => {
+        if (toValue(manual)) {
+          options.immediate = false
+          options.watch = false
+        }
 
-      await beforeCb()
-    },
+        // merge additional options passed to useApiCall
+        _merge(options, additionalOptions)
+      },
+
+      // user callback
+      async () => await beforeCb(),
+    ],
 
     onRequestError: async ({ error }) => {
-      //console.log("[useFetch request error]")
-      //console.dir(error, { colors: true })
-
       await fetchErrorCb(error)
     },
 
     onResponse: async ({ response }) => {
-      //console.log("[useFetch response]")
-      //console.dir(response, { colors: true })
-
       if (response.ok) {
         await successCb(response)
       }
     },
 
     onResponseError: async ({ response }) => {
-      //console.log("[useFetch response error]")
-      //console.dir(response, { colors: true })
-      //console.dir(response.error, { colors: true })
-
-      if (
-        _has(response._data, "fieldError") ||
-        (response.status === 422)
-      ) {
+      if (_has(response._data, "fieldError") || (response.status === 422)) {
         await fieldErrorCb(response)
       } else {
         await apiErrorCb(response)
       }
     },
 
-    transform: (data) => deepConvertKeys(data, _camelCase)
+    transform: (response) => deepConvertKeys(response, _camelCase)
   }
 
-  if (manualFetch) {
-    options.immediate = false
-    options.watch = false
-  }
-
-  // assign fetchOptions properties to options (with overwriting)
-  _assign(options, fetchOptions)
-
-  return useFetch(url, options)
+  return useFetch(url, useFetchOptions)
 }
