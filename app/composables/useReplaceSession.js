70139e83cc735de4bbafe8979b4fcaafcee09842
Jason Gorst <jason.gorst@me.com>
2025-01-12 23:30:14 -0600

    rename useSetSession useReplaceSession

--- app/composables/useReplaceSession.js
+++ app/composables/useReplaceSession.js
@@ -1,11 +1,11 @@
-export async function useSetSession(user) {
+export async function useReplaceSession(sessionData) {
   const { fetch: fetchUserSession, session } = useUserSession()
 
   // call setSessionUser api
   await $fetch(
-    "/api/v1/auth/setSessionUser", {
+    "/api/v1/auth/replaceUserSession", {
       method: "POST",
-      body: JSON.stringify(user)
+      body: JSON.stringify(sessionData)
     })
 
   // refresh the session
