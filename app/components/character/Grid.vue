c92c6be1b683d4fa526152be2e759414905c4bf7
Jason Gorst <jason.gorst@me.com>
2025-02-15 13:38:55 -0600

    remove extra wrapper div around CharacterGrid

--- app/components/character/Grid.vue
+++ app/components/character/Grid.vue
@@ -1,39 +1,37 @@
 <template>
-  <div>
-    <table class="w-full table table-pin-rows table-md table-zebra mb-4">
-      <thead>
-      <tr class="bg-base-300 z-0">
-        <th
-          v-for="attribute of listAttributes"
-          :key="attribute"
-          :data-tip="attributeTooltip(attribute)"
-          class="text-secondary select-none font-light table-cell text-center tooltip tooltip-bottom tooltip-primary tooltip-late tooltip-bottom-near hover:text-primary"
-          @click="updateSort(attribute)"
-        >
-          <span class="uppercase font-bold">
-            {{ _startCase(attribute) }}
-          </span>
+  <table class="w-full table table-pin-rows table-md table-zebra mb-4">
+    <thead>
+    <tr class="bg-base-300 z-0">
+      <th
+        v-for="attribute of listAttributes"
+        :key="attribute"
+        :data-tip="attributeTooltip(attribute)"
+        class="text-secondary select-none font-light table-cell text-center tooltip tooltip-bottom tooltip-primary tooltip-late tooltip-bottom-near hover:text-primary"
+        @click="updateSort(attribute)"
+      >
+        <span class="uppercase font-bold">
+          {{ _startCase(attribute) }}
+        </span>
 
-          <span class="inline-block w-0 overflow-x-visible text-accent">
-            <Icon
-              v-if="sort.attribute === attribute"
-              :name="sort.direction === 'asc' ? 'fa6-solid:arrow-down-a-z' : 'fa6-solid:arrow-down-z-a'"
-              class="w-[1.25em] h-[1em] -mt-[0.1667em]"
-            />
-          </span>
-        </th>
-      </tr>
-      </thead>
+        <span class="inline-block w-0 overflow-x-visible text-accent">
+          <Icon
+            v-if="sort.attribute === attribute"
+            :name="sort.direction === 'asc' ? 'fa6-solid:arrow-down-a-z' : 'fa6-solid:arrow-down-z-a'"
+            class="w-[1.25em] h-[1em] -mt-[0.1667em]"
+          />
+        </span>
+      </th>
+    </tr>
+    </thead>
 
-      <tbody>
-      <CharacterItem
-        v-for="character in personae.filteredCharacters"
-        :key="character.id"
-        :character="character"
-      />
-      </tbody>
-    </table>
-  </div>
+    <tbody>
+    <CharacterRow
+      v-for="character in personae.filteredCharacters"
+      :key="character.id"
+      :character="character"
+    />
+    </tbody>
+  </table>
 </template>
 
 <script setup>
