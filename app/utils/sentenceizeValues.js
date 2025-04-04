6e58662e0d3674e2f73021e7b3d4d1bc63e97123
Jason Gorst <jason.gorst@me.com>
2025-04-03 23:14:45 -0500

    remove unnecessary line breaks

--- app/utils/sentenceizeValues.js
+++ app/utils/sentenceizeValues.js
@@ -1,6 +1,3 @@
 export default function sentenceizeValues(object) {
-  return deepJoinArrays(
-    deepConvertValues(object, sentenceize),
-    " "
-  )
+  return deepJoinValues(deepConvertValues(object, sentenceize), " ")
 }
