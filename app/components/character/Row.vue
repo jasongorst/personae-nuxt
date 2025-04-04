3bdbc438a4038a20a8408d88f3ae627f1094cf28
Jason Gorst <jason.gorst@me.com>
2025-04-03 22:57:15 -0500

    use &nbsp; if attribute is blank

--- app/components/character/Row.vue
+++ app/components/character/Row.vue
@@ -1,15 +1,20 @@
 <template>
-  <tr class="hover:bg-base-300!">
+  <tr
+    class="hover:bg-base-300!"
+    :data-testid="`row_${character.id}`"
+  >
     <td
-      v-for="attribute of listAttributes"
+      v-for="attribute in listAttributes"
       :key="`${character.id}_${attribute}`"
       class="p-0! whitespace-nowrap"
+      :data-testid="`${attribute}_${_kebabCase(character[attribute])}`"
     >
+      <!--suppress HtmlUnknownTarget -->
       <NuxtLink
         :to="`/show-${character.id}`"
         class="inline-block w-full px-3 py-2 xl:px-4 xl:py-3"
       >
-        {{ character[attribute] || '&nbsp;' }}
+        {{ isPresent(character[attribute]) ? character[attribute] : "&nbsp;" }}
       </NuxtLink>
     </td>
   </tr>
