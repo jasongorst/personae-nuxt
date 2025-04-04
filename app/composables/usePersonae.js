b2890b5e7923cd35a35ed8a023773862d6e3dff7
Jason Gorst <jason.gorst@me.com>
2025-04-03 23:07:05 -0500

    convert to composition store API, use lodash _some to simplify isFilterSet getter, use lodash _every to simplify isInFilter action, add hmr support

--- app/composables/usePersonae.js
+++ app/composables/usePersonae.js
@@ -1,7 +1,7 @@
 export const usePersonae = defineStore("personae", () => {
   // state
   const characters = ref([])
-  const filter = ref(() => getBlankFilter())
+  const filter = ref(getBlankFilter())
   const query = ref("")
 
   // getters
@@ -12,15 +12,10 @@ export const usePersonae = defineStore("personae", () => {
 
   const filteredCount = computed(() => filteredCharacters.value.length)
 
-  const isFilterSet = computed(() => {
-    for (const attribute of filterAttributes) {
-      if (isPresent(filter.value[attribute])) {
-        return true
-      }
-    }
-
-    return false
-  })
+  const isFilterSet = computed(() => _some(
+    filterAttributes,
+    (attribute) => isPresent(filter.value[attribute])
+  ))
 
   const totalCount = computed(() => characters.value.length)
   const isLoaded = computed(() => isPositive(totalCount.value))
@@ -50,30 +45,11 @@ export const usePersonae = defineStore("personae", () => {
     }
   }
 
-  // private
-  function getBlankFilter() {
-    return _fromPairs(
-      filterAttributes.map(
-        (attribute) => [ attribute, [] ]
-      )
-    )
-  }
-
   // true if, for each array of attributes in filter, the array is empty or contains character[attribute]
   function isInFilter(character, filter) {
-    let result = true
-
-    // noinspection FunctionWithInconsistentReturnsJS
-    _forEach(filter, (values, attribute) => {
-      if (!isEmpty(values) && !values.includes(character[attribute])) {
-        result = false
-
-        // exit forEach early
-        return false
-      }
-    })
-
-    return result
+    return _every(
+      filter,
+      (values, attribute) => (_isEmpty(values) || _includes(values, character[attribute])))
   }
 
   return {
@@ -92,3 +68,8 @@ export const usePersonae = defineStore("personae", () => {
     sortCharacters
   }
 })
+
+// hmr support
+if (import.meta.hot) {
+  import.meta.hot.accept(acceptHMRUpdate(usePersonae, import.meta.hot))
+}
