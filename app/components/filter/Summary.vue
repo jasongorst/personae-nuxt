196c79621e2e2061d96558850420602f2b1302c5
Jason Gorst <jason.gorst@me.com>
2025-04-03 23:01:22 -0500

    rename CharacterSummary to FilterSummary

--- app/components/filter/Summary.vue
+++ app/components/filter/Summary.vue
@@ -2,6 +2,7 @@
   <div
     v-if="isFilterSet && !showFilter"
     class="bg-base-200 flex flex-row flex-wrap gap-2 px-4 py-2 items-center"
+    data-testid="summary"
   >
     <template
       v-for="(attributeValues, attribute) in filter"
@@ -11,10 +12,12 @@
         v-for="attributeValue in attributeValues"
         :key="attributeValue"
         class="badge badge-secondary border-none rounded-full gap-0 pl-0 whitespace-nowrap"
+        data-testid="value"
       >
         <button
           class="btn btn-secondary shadow-none pl-2 pr-1 rounded-l-full max-h-full"
           @click="removeFilter(attribute, attributeValue)"
+          data-testid="button"
         >
           <Icon
             name="ph:x-bold"
