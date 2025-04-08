3482d7524da7a549c1064ffc157f635aca94e052
Jason Gorst <jason.gorst@me.com>
2025-04-08 14:29:42 -0500

    add e2e and mocks folders to vitest exclude list

--- vitest.config.js
+++ vitest.config.js
@@ -3,6 +3,8 @@ import { defineVitestConfig } from '@nuxt/test-utils/config'
 export default defineVitestConfig({
   test: {
     environment: "nuxt",
+    include: "./test/**/*.test.js",
+    exclude: [ "./test/e2e/**", "./test/mocks/**" ],
     globals: true
   }
 })
